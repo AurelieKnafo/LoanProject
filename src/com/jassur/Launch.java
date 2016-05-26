bb2c43a2e877b31240ef7d2be69394ca7422aac8
Michael Azerhad <m.azerhad@wealcome.com>
2016-05-26 22:33:12 +0200

    adding packages

--- src/com/jassur/Launch.java
+++ src/com/jassur/Launch.java
@@ -1,12 +1,17 @@
 package com.jassur;
 
+import com.jassur.controllers.LoanCostsCalculator;
+import com.jassur.models.Loan;
+import com.jassur.views.LoanCostsReporter;
+
 class Launch {
 
     public static void main(String[] args) {
         Loan withVariationSample = Loan.withVariation(10, 10000, 1, 0.9995);
         Loan withoutVariationSample = Loan.withoutVariation(10, 10000, 1);
-        new LoanCostsReporter().report(withVariationSample.calculateCost());
-        new LoanCostsReporter().report(withoutVariationSample.calculateCost());
+        LoanCostsCalculator loanCostsCalculator = new LoanCostsCalculator();
+        new LoanCostsReporter().report(loanCostsCalculator.calculate(withVariationSample));
+        new LoanCostsReporter().report(loanCostsCalculator.calculate(withVariationSample));
     }
 
 }
